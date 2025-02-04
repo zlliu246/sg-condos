27929c0bc07a1ae89f7f52aca435ddc1fb1bcaaf
zlliu246 <zlliu246@gmail.com>
2025-02-04 23:34:54 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+const headerLinks = [
+  {name:'Home', path:'/'},
+  {name:'About', path:'/about'},
+];
+
+</script>
+
+<template>
+  <header class="p-2">
+    <span v-for="row of headerLinks" :key="row.name">
+      <RouterLink :to="row.path" class="topbar-name">{{row.name}}</RouterLink>
+    </span>
+  </header>
+
+  <RouterView />
+
+  <footer>
+    <br>
+    <br>
+  </footer>
+</template>
+
+<style>
+
+header {
+  box-shadow: 0 0 1px 1px #ccc!important;
+  text-align: right; 
+  margin-bottom:3px;
+}
+
+header span {
+  margin: 0.5em;
+}
+
+.topbar-name {
+  text-decoration: none !important;
+}
+
+.topbar-name:hover {
+  font-weight: 500;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    this is the home page
+  </div>
+</template>
